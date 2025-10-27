3f4c6792e72c30ffe579fff7c92b7229a6ed9e6f
HungLT <hunglt@evnfc.vn>
2025-10-27 15:57:37 +0700

    adapter

--- adapter.ts
+++ adapter.ts
@@ -22,3 +22,18 @@ const googleService = new GoogleMapService();
 // inject googleService vào để lấy coordinates
 const app = new LocationApp(googleService);
 app.showLocation("TP. Hồ Chí Minh");
+
+
+// PROBLEMS
+// API mới từ MapBox
+class MapBoxService {
+  // không có hàm getCoordinates
+  getLatLongByPlaceName(place: string) {
+    console.log(`🗺️ MapBox tìm tọa độ cho: ${place}`);
+    return { latitude: 10.762622, longitude: 106.660172 };
+  }
+}
+
+const mapBoxService = new MapBoxService();
+const app1 = new LocationApp(mapBoxService); // LỖI [Property 'getCoordinates' is missing in type 'MapBoxService']
+app1.showLocation("TP. Hồ Chí Minh");
